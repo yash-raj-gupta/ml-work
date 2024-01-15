ffd4e4fe63e87ee003a83fa80bd3eb6f5df4f137
Yash Raj Gupta <yashraj.guptadev@gmail.com>
2024-01-16 00:15:23 +0530

    minor tweeks

--- Frontend/src/components/Analysis.js
+++ Frontend/src/components/Analysis.js
@@ -8,19 +8,32 @@ import mallActivityData from '../log_mall_activity.json'
 
 const DelayedDescription = ({ number, description, delay }) => {
   const [showDescription, setShowDescription] = useState(false);
+  const [formattedTime, setFormattedTime] = useState('');
+
+  const convertToMinutesAndSeconds = (delay) => {
+    const delayInSeconds = Math.floor(delay / 1000);
+    const minutes = Math.floor(delayInSeconds / 60);
+    const seconds = delayInSeconds % 60;
+
+    const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formattedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+  };
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowDescription(true);
     }, delay);
-
+    setFormattedTime(convertToMinutesAndSeconds(delay));
     return () => clearTimeout(timer);
   }, [delay]);
-
+  
   return (
     <>
     {showDescription && <div className="mx-2 my-3 p-2 bg-[#FFFFFF] rounded-xl drop-shadow-xl">
-       <h2 className="text-lg font-semibold mb-2">{Math.floor(delay/1000)} seconds</h2>
+       <h2 className="text-lg font-semibold mb-2">{formattedTime}</h2>
       <div className="border-t border-gray-300 mb-2"></div>
       <p className="font-normal">{description}</p>
     </div>}
@@ -63,8 +76,7 @@ const Analysis = () => {
             <div className="rounded-3xl overflow-hidden drop-shadow-xl">
               <ReactPlayer
               url={videos[videos.length -1].videoUrl}
-                autoplay
-                controls
+                playing={true}
                 width="539px"
                 height="292px"
               />
